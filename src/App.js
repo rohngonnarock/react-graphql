a08dfda432ca2fe8aaac65b2b5cbefb0885d9d46
Rahul Prajapati <rohngonnarock@gmail.com>
2022-03-03 10:01:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState, useEffect } from "react";
+
+import { gql, useQuery } from "@apollo/client";
+
+const getData = gql`
+  {
+    users {
+      firstName
+      lastName
+      email
+    }
+  }
+`;
 
 function App() {
+  const { error, loading, data } = useQuery(getData);
+  const [gData, setGData] = useState();
+
+  useEffect(() => {
+    if (data && data.users) {
+      setGData(data.users);
+    }
+  }, [data]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {gData &&
+          gData.map &&
+          gData.map((user) => (
+            <div>
+              {" "}
+              {user.firstName} : {user.lastName} : {user.email}{" "}
+            </div>
+          ))}
       </header>
     </div>
   );
